55776f8d440b7eca869d3e0705f718e86a2885c7
Tusharmaji135 <135tusharmaji@gmail.com>
2024-07-15 21:13:20 +0530

    hi

---
+++ script.js
@@ -0,0 +1,42 @@
+const apiKey = "28026b6fe9a6b7473faf392cd0353c95";
+
+const apiURL =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector("#searchcity");
+const searchButton = document.querySelector("#sbutton");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city) {
+  const response = await fetch(apiURL + city + `&appid=${apiKey}`);
+
+  if (response.status == 404) {
+    document.querySelector(".error").style.display = "none";
+  } else {
+    let data = await response.json();
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML =
+      Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+    if (data.weather[0].main == "Clouds") {
+      weatherIcon.src = "images/clouds.png";
+    } else if (data.weather[0].main == "Clear") {
+      weatherIcon.src = "images/clear.png";
+    } else if (data.weather[0].main == "Rain") {
+      weatherIcon.src = "images/rain.png";
+    } else if (data.weather[0].main == "Drizzle") {
+      weatherIcon.src = "images/drizzle.png";
+    } else if (data.weather[0].main == "Mist") {
+      weatherIcon.src = "images/mist.png";
+    } else if (data.weather[0].main == "Haze") {
+      weatherIcon.src = "//ssl.gstatic.com/onebox/weather/64/fog.png";
+    }
+  }
+}
+searchButton.addEventListener("click", (e) => {
+  checkWeather(searchBox.value);
+});
